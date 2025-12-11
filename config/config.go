a6a5087a138cf9e18086c0fdd0cfd2219d929887
madeindreams <madeindreams@protonmail.com>
2025-12-11 02:58:13 -0800

    embed config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/fatih/structs"
@@ -9,28 +10,44 @@ import (
 	"github.com/spf13/viper"
 )
 
+// ParseConfig behaves like before (no embedded defaults).
+// It just forwards to ParseConfigWithEmbedded with nil.
 func ParseConfig[T interface{}](configFilePaths []string) (*T, error) {
+	return ParseConfigWithEmbedded[T](configFilePaths, nil)
+}
+
+// ParseConfigWithEmbedded tries to load config from disk,
+// and if the file is NOT found, falls back to embeddedYAML (if provided).
+func ParseConfigWithEmbedded[T interface{}](configFilePaths []string, embeddedYAML []byte) (*T, error) {
 	for _, v := range configFilePaths {
 		viper.AddConfigPath(v)
 	}
-	viper.SetConfigName("config.yaml")
+
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 
-	err := bindAllConfigKeys[T]()
-	if err != nil {
+	if err := bindAllConfigKeys[T](); err != nil {
 		return nil, err
 	}
 
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
-	err = viper.ReadInConfig()
+
+	err := viper.ReadInConfig()
 	if err != nil {
-		return nil, err
+		var nfErr viper.ConfigFileNotFoundError
+		// ✅ use errors.As instead of a direct type assertion
+		if errors.As(err, &nfErr) && len(embeddedYAML) > 0 {
+			if err2 := viper.ReadConfig(bytes.NewReader(embeddedYAML)); err2 != nil {
+				return nil, errors.Wrap(err2, "failed to load embedded default config")
+			}
+		} else {
+			return nil, err
+		}
 	}
 
 	var c *T
-	err = viper.Unmarshal(&c)
-	if err != nil {
+	if err := viper.Unmarshal(&c); err != nil {
 		return nil, errors.Wrap(err, "Unable to decode into struct")
 	}
 
@@ -50,10 +67,9 @@ func bindAllConfigKeys[T interface{}]() error {
 		return errors.Wrap(err, "Unable to flatten config")
 	}
 
-	// Bind each conf fields to environment vars
+	// Bind each conf field to environment vars
 	for key := range flat {
-		err := viper.BindEnv(key)
-		if err != nil {
+		if err := viper.BindEnv(key); err != nil {
 			return errors.Wrapf(err, "Unable to bind env var: %s", key)
 		}
 	}
