dbec149daeadd76fa6129ada182ea70208fe0642
Ian Dorion <madeindreams@protonmail.com>
2026-01-17 13:26:55 -0500

    Create constants.go

---
+++ qa/constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	HeaderAuthorization           = "Authorization"
+	HeaderQuantumAuthCanonicalB64 = "X-QuantumAuth-Canonical-B64"
+	HeaderQuantumAuth             = "QuantumAuth"
+)
